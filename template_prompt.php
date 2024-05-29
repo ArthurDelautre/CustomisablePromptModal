75f765b69b301d9f9734c3ea5918da679b6812ed
ArthurDelautre <114985589+ArthurDelautre@users.noreply.github.com>
2024-05-29 11:38:07 +0200

    create template_prompt.php

---
+++ template_prompt.php
@@ -0,0 +1,12 @@
+<div id="customPrompt" class="modal">
+    <div class="modal-content">
+        <p id="titlePromptMessage"></p>
+        <p id="subtitlePromptMessage"></p>
+        <div class="button-container">
+            <button id="confirmChoice1"></button>
+            <button id="confirmChoice2"></button>
+        </div>
+    </div>
+</div>
+
+<link rel="stylesheet" type="text/css" href="style_prompt.css">
